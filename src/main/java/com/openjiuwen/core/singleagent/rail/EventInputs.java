b2ef45ee2d2061bb3ef0c04ee0ad8e7336eab69b
ttming <tianmingyong@huawei.com>
2026-03-18 11:24:45 +0800

    待修复singleagent模块

--- src/main/java/com/openjiuwen/core/singleagent/rail/EventInputs.java
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
-package com.openjiuwen.core.singleagent.rail;
-
-/**
- * Marker interface for typed event inputs.
- */
-public interface EventInputs {
-}
