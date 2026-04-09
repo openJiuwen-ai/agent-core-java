69556ae6dd6a8218bd753db86c8cdda0b63e8e32
tianmingyong <tianmingyong@huawei.com>
2026-04-09 10:40:46 +0800

    去除多余空格

--- src/main/java/com/openjiuwen/core/foundation/llm/model_clients/DefaultModelClientFactories.java
+++ src/main/java/com/openjiuwen/core/foundation/llm/model_clients/DefaultModelClientFactories.java
@@ -1,6 +1,6 @@
-  /*
-   * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
-   */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+ */
 
 package com.openjiuwen.core.foundation.llm.model_clients;
 
--- src/main/java/com/openjiuwen/extensions/context_evolver/summary/task/ace/CurateOp.java
+++ src/main/java/com/openjiuwen/extensions/context_evolver/summary/task/ace/CurateOp.java
@@ -1,6 +1,6 @@
-  /*
-   * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
-   */
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2026-2026. All rights reserved.
+ */
 
 package com.openjiuwen.extensions.context_evolver.summary.task.ace;
 
